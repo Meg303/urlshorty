b6f29ec9a0d297280eceb4071d8773133526f8b3
Meg303 <megrhinehart@gmail.com>
2014-03-07 11:05:46 -0500

    repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urlshorty::Application.config.secret_key_base = '7a149e413f2f001dea040e9fb69c5fce58255f2ef4f42823386a9f3f0f30280725908f54e4a196ce099098daefe87e535ea98b2547f1f6c810dad657f9dee0df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Urlshorty::Application.config.session_store :cookie_store, key: '_urlshorty_session'
